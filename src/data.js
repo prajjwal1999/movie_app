30b4e8300daac80775f92c9e8aeb581f033bf4e3
Prajjwal Singh <prajjwal875.com>
2020-12-23 18:41:05 +0530

    strucure

---
+++ src/data.js
@@ -0,0 +1,385 @@
+export const data=[
+   
+    {
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },
+    {
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },{
+        Title:'The Avengers',
+        Year:'2012',
+        Rated:'PG-13',
+        Released:'04 May 2012',
+        Runtime:'143 min',
+        Genre:'Action,Adventure, Sci-Fi',
+        Director:'JossWhedon',
+        Writer:'Joss Whedon (screenplay),Zak Penn (story),JossWhedon(story)',
+        Plot:"Earth's mightiest heroes must come together an dlearn to fight as a team if they are going to stay alive",
+        Languages:'English,Russian ,Hindi',
+        Country:'USA',
+        Awards:'Nominated for 1 Oscar, Another 38 wins and 79 nominations',
+        Poster:'https://wallpapercave.com/wp/wp4770368.jpg',
+        Rating:[
+            {
+                Source:'Internet Movie Database',
+                Value:''
+            },
+            {
+                Source:'Rotten Tomatoes',
+                Value:'91'
+            },
+            {
+                Source:'Metacritics',
+                Value:'96/100'
+            }
+        ],
+        Metascore:'69',
+        imdbRating:'8.0',
+        imdbVotes:'1,216,895',
+        imdbID:'tt0848228',
+        Type:'movie',
+        DVD:'25 Sep 2012',
+        BoxOffice:'$623,279,547',
+        Production:'Walt Disney Pictures',
+        Website:'N/A',
+        Response:'True'
+    },
+]
