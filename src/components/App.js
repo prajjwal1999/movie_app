1f70531149f8755661cdfbab35d5e2df42c71f90
Prajjwal Singh <prajjwal875.com>
2021-01-23 15:02:50 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,72 @@
-import React from 'react'
-import Navbar from'./Navbar'
-import MovieCard from './MovieCard'
-import {data} from '../data'
-import { render } from '@testing-library/react';
-import {addMovies,setShowFavorites} from '../actions'
+import React from "react";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+import { data } from "../data";
+import { render } from "@testing-library/react";
+import { addMovies, setShowFavorites } from "../actions";
 class App extends React.Component {
-  componentDidMount(){
-    const {store}=this.props;
-    store.subscribe(()=>{
-      console.log('UPDATED');
+  componentDidMount() {
+    const { store } = this.props;
+    store.subscribe(() => {
+      console.log("UPDATED");
       this.forceUpdate();
-
     });
-    store.dispatch((addMovies(data)))
-    console.log('state',this.props.store.getState());
+    store.dispatch(addMovies(data));
+    console.log("state", this.props.store.getState());
   }
-  isMovieFavorite=(movie)=>{
-    const {movies}=this.props.store.getState();
-    const index=movies.favorites.indexOf(movie);
-    if(index!==-1)
-    {
-    return true;
+  isMovieFavorite = (movie) => {
+    const { movies } = this.props.store.getState();
+    const index = movies.favorites.indexOf(movie);
+    if (index !== -1) {
+      return true;
     }
     return false;
-
-  }
-  onChangeTab=(val)=>{
+  };
+  onChangeTab = (val) => {
     this.props.store.dispatch(setShowFavorites(val));
-  }
-  
-  render(){
-    const {movies,search}=this.props.store.getState();
-    const {list,favorites,showFavorites}=movies;
+  };
+
+  render() {
+    const { movies, search } = this.props.store.getState();
+    const { list, favorites, showFavorites } = movies;
 
     console.log(this.props.store.getState());
-    const displayMovies=showFavorites?favorites:list;
-  return (
-    <div className="App">
-      <Navbar dispatch={this.props.store.dispatch} search={search}/>
-      <div className="main">
-        <div className="tabs">
-          <div className={`tab ${showFavorites?'':'acive-tabs'}`} onClick={()=>this.onChangeTab(false)}>Movies</div>
-          <div className={`tab ${showFavorites?'acive-tabs':''}`}   onClick={()=>this.onChangeTab(true)}>Favorite</div>
+    const displayMovies = showFavorites ? favorites : list;
+    return (
+      <div className="App">
+        <Navbar dispatch={this.props.store.dispatch} search={search} />
+        <div className="main">
+          <div className="tabs">
+            <div
+              className={`tab ${showFavorites ? "" : "acive-tabs"}`}
+              onClick={() => this.onChangeTab(false)}
+            >
+              Movies
+            </div>
+            <div
+              className={`tab ${showFavorites ? "acive-tabs" : ""}`}
+              onClick={() => this.onChangeTab(true)}
+            >
+              Favorite
+            </div>
+          </div>
+          <div className="list">
+            {displayMovies.map((movie, index) => (
+              <MovieCard
+                movie={movie}
+                key={"movies-${index}"}
+                dispatch={this.props.store.dispatch}
+                isFavorite={this.isMovieFavorite(movie)}
+              />
+            ))}
+          </div>
+          {displayMovies.length === 0 ? (
+            <div className="no-movies">No movies to display..!!</div>
+          ) : null}
         </div>
-        <div className="list">
-          {displayMovies.map((movie,index)=>(
-          <MovieCard movie={movie} key={'movies-${index}'} dispatch={this.props.store.dispatch} isFavorite={this.isMovieFavorite(movie)}/>
-          ))}
-        </div>
-        {displayMovies.length===0 ? <div className="no-movies">No movies to display..!!</div>:null}
       </div>
-      
-    
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,78 +1,82 @@
-import { combineReducers } from 'redux';
-import {ADD_FAVORITE, ADD_MOVIES, ADD_MOVIE_TO_LIST, ADD_SEARCH_RESULT, REMOVE_FROM_FAVORITES,SET_SHOW_FAVORITES} from '../actions/index'
-const initialMoviesState={
-    list:[],
-    favorites:[],
-    showFavorites:false
-}
-export function movies(state=initialMoviesState,action){
-    // if(action.type===ADD_MOVIES)
-    // {
-
-    //     return {
-    //         ...state,
-    //         list:action.movies
-    //     }
-    // }
-    
-    //     return state;
-    switch(action.type){
-        case ADD_MOVIES:
-            return{
-                ...state,
-                list:action.movies
-            }
-            case ADD_FAVORITE:
-                return{
-                    ...state,
-                    favorites:[action.movie, ...state.favorites]
-                }
-            case REMOVE_FROM_FAVORITES:
-                const filteredArray=state.favorites.filter(
-                    movie=>movie.Title!==action.movie.Title
-                );
-                return{
-                    ...state,
-                    favorites:filteredArray
-
-                };
-            case SET_SHOW_FAVORITES:
-                return{
-                    ...state,
-                   showFavorites: action.val
-                }
-                case ADD_MOVIE_TO_LIST:
-                    return{
-                        ...state,
-                        list:[action.movie,...state.list]
-                    }
-                default:
-                    return state;
-    }
-}
-
-const initialSearchState={
-    result:{},
-    showSearchResults:false
+import { combineReducers } from "redux";
+import {
+  ADD_FAVORITE,
+  ADD_MOVIES,
+  ADD_MOVIE_TO_LIST,
+  ADD_SEARCH_RESULT,
+  REMOVE_FROM_FAVORITES,
+  SET_SHOW_FAVORITES,
+} from "../actions/index";
+const initialMoviesState = {
+  list: [],
+  favorites: [],
+  showFavorites: false,
 };
-export function search(state=initialSearchState,action){
-    
-    switch(action.type){
-        case ADD_SEARCH_RESULT:
-            return{
-                ...state,
-                result:action.movie,
-                showSearchResults:true
-            }
-            case ADD_MOVIE_TO_LIST:
-                return{
-                    ...state,
-                   showSearchResults:false
-                }
-            default:
-                return state
-    }
-    
+export function movies(state = initialMoviesState, action) {
+  // if(action.type===ADD_MOVIES)
+  // {
+
+  //     return {
+  //         ...state,
+  //         list:action.movies
+  //     }
+  // }
+
+  //     return state;
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies,
+      };
+    case ADD_FAVORITE:
+      return {
+        ...state,
+        favorites: [action.movie, ...state.favorites],
+      };
+    case REMOVE_FROM_FAVORITES:
+      const filteredArray = state.favorites.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+      return {
+        ...state,
+        favorites: filteredArray,
+      };
+    case SET_SHOW_FAVORITES:
+      return {
+        ...state,
+        showFavorites: action.val,
+      };
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        list: [action.movie, ...state.list],
+      };
+    default:
+      return state;
+  }
+}
+
+const initialSearchState = {
+  result: {},
+  showSearchResults: false,
+};
+export function search(state = initialSearchState, action) {
+  switch (action.type) {
+    case ADD_SEARCH_RESULT:
+      return {
+        ...state,
+        result: action.movie,
+        showSearchResults: true,
+      };
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        showSearchResults: false,
+      };
+    default:
+      return state;
+  }
 }
 // const initialRootState={
 //     movies:initialMoviesState,
@@ -86,6 +90,6 @@ export function search(state=initialSearchState,action){
 //     }
 // }
 export default combineReducers({
-    movies,
-    search
+  movies,
+  search,
 });
